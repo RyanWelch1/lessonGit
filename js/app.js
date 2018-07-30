dc837488bce96437dc8f37d338b6d30f7e885450
Ryan Welch <ryan.welch1@live.com>
2018-07-29 23:22:23 -0500

    update

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ document.body.appendChild(myPara);
 //method has to be called with two arguments:
 //the location of the HTML
 //the HTML text that is going to be inserted
-//
+//demo
 
 const mainHeadingTwo = document.querySelector('.testThree');
 const htmlTextToAdd = '<h2>Skydiving is fun!</h2>';
